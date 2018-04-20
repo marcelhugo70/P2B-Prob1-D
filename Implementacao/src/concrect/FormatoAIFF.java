24af4d1f4fb676dfe3684a054142db9d7d96cc83
Luciane Tedesco <lucianetedesco25@gmail.com>
2018-04-19 22:03:42 -0300

    Criação classe de testes, métodos reproduzirSimples e pararSimples e ajustes finos

--- Implementacao/src/concrect/FormatoAIFF.java
+++ Implementacao/src/concrect/FormatoAIFF.java
@@ -1,62 +1,58 @@
-/**
- * 
- */
+
 package concrect;
 
 import interfaces.FormatoAudio;
 import problema1.AIFFSuperPlayer;
 
 /**
- * @author Luciane
+ * @authors Gustavo Henrique Spiess, Gustavo Korbes Heinen, Luciane Tedesco e
+ *          Matheus Mahnke
  *
  */
-public class FormatoAIFF  implements FormatoAudio {
+public class FormatoAIFF implements FormatoAudio {
 
-    private AIFFSuperPlayer player;
-    private int cursor = 0;
+	private AIFFSuperPlayer player;
+	private int cursor = 0;
 
-    public FormatoAIFF() {
-    }
-
-    @Override
-    public void abrir(String audio) {
-        this.player = new AIFFSuperPlayer(audio);
-    }
+	@Override
+	public void abrir(String audio) {
+		this.player = new AIFFSuperPlayer(audio);
+	}
 
 	public void reproduzir() {
-        this.player.setCursor(this.cursor);
-        this.player.play();
-    }
+		this.player.setCursor(this.cursor);
+		this.player.play();
+	}
 
-    @Override
-    public void pausar() {
-        this.cursor = this.player.pause();
-    }
+	@Override
+	public void pausar() {
+		this.cursor = this.player.pause();
+	}
 
-    @Override
-    public void parar() {
-        this.player.stop();
-        this.cursor = 0;
-    }
+	@Override
+	public void parar() {
+		this.player.stop();
+		this.cursor = 0;
+	}
 
-    @Override
-    public void avancar(int segundo) {
-        this.pausar();
-        this.cursor += segundo;
-        if (this.cursor < 0){
-            this.cursor = 0;
-        }
-        this.reproduzir();
-    }
+	@Override
+	public void avancar(int segundo) {
+		this.pausar();
+		this.cursor += segundo;
+		if (this.cursor < 0) {
+			this.cursor = 0;
+		}
+		this.reproduzir();
+	}
 
-    @Override
-    public void retornar(int segundo) {
-        int negSegundos = segundo * -1;
-        this.avancar(negSegundos);
-    }
+	@Override
+	public void retornar(int segundo) {
+		int negSegundos = segundo * -1;
+		this.avancar(negSegundos);
+	}
 
 	@Override
 	public void liberar() {
-        this.player.release();
+		this.player.release();
 	}
 }
--- Implementacao/src/interfaces/FormatoAudio.java
+++ Implementacao/src/interfaces/FormatoAudio.java
@@ -1,20 +1,54 @@
 package interfaces;
 
-
+/**
+ * @authors Gustavo Henrique Spiess, Gustavo Korbes Heinen, Luciane Tedesco e
+ *          Matheus Mahnke
+ *
+ */
 public interface FormatoAudio {
-	
-	public void abrir(String audio);
-	
-	public void reproduzir();
-	
-	public void pausar();
-	
-	public void parar();
-	
-	public void avancar(int segundo);
-	
-	public void retornar(int segundo);
-	
-	public void liberar();
+
+    /**
+     * O método abrir(String) carrega um arquivo de áudio na memória, recebendo como parâmetro o nome do arquivo.
+     * 
+     * @param audio Nome do arquivo.
+     */
+    public void abrir(String audio);
+
+    /**
+     * O método reproduzir() reproduz o arquivo de áudio que foi carregado na memória pelo método abrir(). A reprodução
+     * ocorre a partir do início do arquivo (na primeira vez) ou no ponto onde a reprodução tenha sido pausada.
+     */
+    public void reproduzir();
+
+    /**
+     * O método pausar() para a reprodução do arquivo.
+     */
+    public void pausar();
+
+    /**
+     * O método parar() para a reprodução e retorna para o início do arquivo.
+     */
+    public void parar();
+
+    /**
+     * O método avançar(int) recebe como parâmetro a quantidade de segundos que devem ser avançados no arquivo a partir
+     * do ponto atual.
+     * 
+     * @param segundo Quantidade de segundos que devem ser avançados.
+     */
+    public void avancar(int segundo);
+
+    /**
+     * O método retornar(int) recebe como parâmetro a quantidade de segundos que devem ser retrocedidos no arquivo a
+     * partir do ponto atual.
+     * 
+     * @param segundo Quantidade de segundos que devem ser retrocedidos.
+     */
+    public void retornar(int segundo);
+
+    /**
+     * O método liberar() fecha o arquivo e libera a memória.
+     */
+    public void liberar();
 
 }
